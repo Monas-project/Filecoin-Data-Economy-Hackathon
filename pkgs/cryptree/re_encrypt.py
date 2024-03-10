04e92409c529a315ab531343fc078d6e02bf8aa3
yuseirf <82315017+YuseiWhite@users.noreply.github.com>
2024-03-09 20:48:54 -0800

    Add re_encrypt_and_update function to recursively re-encrypt to the leaf node and upload it again to IPFS

--- pkgs/cryptree/re_encrypt.py
+++ pkgs/cryptree/re_encrypt.py
@@ -1,5 +1,7 @@
+from base64 import urlsafe_b64encode
 from cryptography.fernet import Fernet
 from datetime import datetime
+import json
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
@@ -58,3 +60,39 @@ class CryptTreeNode(BaseModel):
         cipher_suite = Fernet(key)
         decrypted_data = cipher_suite.decrypt(ipfs_client.cat(encrypted_data_cid))
         return decrypted_data
+
+    def re_encrypt_and_update(self, ipfs_client: FakeIPFS):
+        if not self.is_leaf:
+            decrypted_metadata = self.decrypt_data(self.metadata_cid, ipfs_client)
+            decrypted_metadata_dict = json.loads(decrypted_metadata.decode())
+            self.metadata = decrypted_metadata_dict
+            # ここから再暗号化
+            new_subfolder_key = Fernet.generate_key()
+            # 新しいsubfolder_keyを更新, update keys
+            self.keydata.subfolder_key = urlsafe_b64encode(new_subfolder_key).decode()
+            # 再暗号化
+            cipher_suite = Fernet(new_subfolder_key)
+            encrypted_metadata = cipher_suite.encrypt(json.dumps(self.metadata, default=datetime_converter).encode())
+            # 新しいCIDを取得して更新
+            new_metadata_cid = ipfs_client.add_bytes(encrypted_metadata)
+            self.metadata_cid = new_metadata_cid
+
+        for child in self.children:
+            child.re_encrypt_and_update(ipfs_client)
+
+        if self.is_leaf:
+            decrypted_filedata = self.decrypt_data(self.metadata_cid, ipfs_client).decode()
+            # fileはtextデータを想定
+            new_file_key = Fernet.generate_key()
+            self.keydata.file_key = urlsafe_b64encode(new_file_key).decode()
+            cipher_suite = Fernet(new_file_key)
+            encrypted_file_data = cipher_suite.encrypt(decrypted_filedata.encode())
+            new_file_cid = ipfs_client.add_bytes(encrypted_file_data)
+            self.metadata.file_cid = new_file_cid
+
+        # 親ノードが存在する場合は、child_infoを更新
+        if self.parent:
+            self.parent.update_child_info(self.metadata.metadata_cid, new_subfolder_key, ipfs_client)
+
+    def update_child_info(self, cid: str, new_subfolder_key: bytes, ipfs_client: FakeIPFS):
+        pass

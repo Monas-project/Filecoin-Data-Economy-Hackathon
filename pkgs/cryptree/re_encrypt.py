4ea26a01a77e577bfa179e2a12c9a5953cf6256d
yuseirf <82315017+YuseiWhite@users.noreply.github.com>
2024-03-09 20:08:49 -0800

    Add updated Keydata class

--- pkgs/cryptree/re_encrypt.py
+++ pkgs/cryptree/re_encrypt.py
@@ -17,3 +17,12 @@ class Metadata(BaseModel):
     created_at: datetime = Field(default_factory=datetime.now)
     file_cid: Optional[str] = None
     child_info: List[ChildNodeInfo] = []
+
+# TODO: データ構造を持つクラスとしてこれがベストなのかを考える
+class KeyData(BaseModel):
+    # TODO: 少なくとも1つのキーを持っていなかったらエラーが出るようにする, root関連がわかっていない
+    root_id: Optional[str] = None
+    root_key: Optional[str] = None
+    file_key: Optional[str] = None
+    # Nodeは必ずKeyDataを持つのでsubfolder_keyをここで管理
+    subfolder_key: Optional[str] = None

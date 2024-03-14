8f344f79d80158f7a59110364b4e93b321357708
somasekimoto <0421.soma@gmail.com>
2024-03-14 22:24:34 +0900

    fix: updaten_node

--- pkgs/cryptree/crypt_tree_node.py
+++ pkgs/cryptree/crypt_tree_node.py
@@ -44,39 +44,25 @@ class CryptTreeNode(CryptTreeNodeModel):
         if file_data and file_cipher_suite:
             enc_file_data = file_cipher_suite.encrypt(file_data)
             file_cid = client.add_bytes(enc_file_data)
-            print("file_cid")
-            print(file_cid)
             metadata.file_cid = file_cid
             metadata.enc_file_key = sk_cipher_suite.encrypt(file_key).decode()
-        
-        print("metadata")
-        print(metadata)
 
         # メタデータを暗号化してIPFSにアップロード
         enc_metadata = sk_cipher_suite.encrypt(metadata.model_dump_json().encode())
-        print("enc_metadata")
-        print(enc_metadata)
         cid = client.add_bytes(enc_metadata)
 
         # # ルートノードの新規作成かどうかを判定
         if parent is None:
-            print("insert_root_info")
             insert_root_info(owner_id, cid, subfolder_key)
         else:
-            print("parent.metadata")
             child_info: ChildNodeInfo = {
                 "cid": cid,
                 "sk": subfolder_key
             }
             parent.metadata.child_info.append(child_info)
             parent_enc_metadata = parent.encrypt_metadata()
-            print("parent_enc_metadata")
-            print(parent_enc_metadata)
             parent_new_cid = client.add_bytes(parent_enc_metadata)
-            print("parent_new_cid")
-            print(parent_new_cid)
             CryptTreeNode.update_all_nodes(parent.metadata.owner_id, parent_new_cid, parent.subfolder_key)
-            print("done")
 
         # インスタンスの作成と返却
         return cls(
@@ -85,50 +71,30 @@ class CryptTreeNode(CryptTreeNodeModel):
         )
     
     def encrypt_metadata(self) -> bytes:
-        print("self.subfolder_key")
-        print(self.subfolder_key)
-        print("self.metadata")
-        print(self.metadata)
-        print("self.metadata.model_dump_json()")
-        print(self.metadata.model_dump_json())
         return Fernet(self.subfolder_key).encrypt(self.metadata.model_dump_json().encode())
 
     @classmethod
     def update_all_nodes(cls, address: str, new_cid: str, target_subfolder_key: bytes):
-        print("update_all_nodes")
         root_id, root_key = get_root_info(address)
-        print("root_id")
-        # root_node_enc_metadata = client.cat(root_id)
-        print("root_node_enc_metadata")
-        # root_node_metadata = json.loads(Fernet(root_key).decrypt(root_node_enc_metadata).decode())
-        print("root_node_metadata")
         root_node = cls.get_node(root_id, root_key)
-        print("root_node")
 
         def update_root_callback(address, new_root_id):
-            print("update_root_callback")
             update_root_id(address, new_root_id)
             
         if root_node.subfolder_key == target_subfolder_key:
-            # root_node.metadata = new_cid
-            # enc_metadata = root_node.encrypt_metadata()
-            # new_cid = client.add_bytes(enc_metadata)
-            print("callbackinggg")
             update_root_callback(address, new_cid)
         else:
             cls.update_node(root_node, address, target_subfolder_key, new_cid, update_root_callback)
 
     @classmethod
-    def update_node(cls, node, address: str, target_subfolder_key: bytes, new_cid: str, callback):
+    def update_node(cls, node, address: str, target_subfolder_key: str, new_cid: str, callback):
         child_info = node.metadata.child_info
-        print("child_info")
-        print(child_info)
         for index, child in enumerate(child_info):
-            if child.sk == target_subfolder_key:
+            child_subfolder_key = child.sk.decode()
+            if child_subfolder_key == target_subfolder_key:
                 node.metadata.child_info[index].cid = new_cid
                 enc_metadata = node.encrypt_metadata()
                 new_cid = client.add_bytes(enc_metadata)
-                print("callbacking")
                 callback(address, new_cid)
                 break
             else:

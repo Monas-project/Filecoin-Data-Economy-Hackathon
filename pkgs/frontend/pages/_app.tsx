2f5f42222f4e6ed24edcea9b68c430dc6d598979
K.N <102404189+v0w0v-kaho@users.noreply.github.com>
2024-03-21 03:35:44 +0900

    Add UI

--- pkgs/frontend/pages/_app.tsx
+++ pkgs/frontend/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { getEnv } from "@/utils/getEnv";
 import {
+<<<<<<< HEAD
   RainbowKitProvider,
   darkTheme,
   getDefaultWallets,
@@ -12,6 +13,37 @@ import { filecoinCalibration } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import "../styles/globals.css";
 import { ResponseData } from "./api/env";
+=======
+  filecoinCalibration
+} from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import '../styles/globals.css';
+
+const {
+  chains,
+  publicClient,
+  webSocketPublicClient
+} = configureChains(
+  [
+    filecoinCalibration,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [filecoinCalibration] : []),
+  ],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Monas',
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!,
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  // autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+>>>>>>> 30f3b97 (Add UI)
 
 /**
  * MyApp Component
@@ -19,6 +51,7 @@ import { ResponseData } from "./api/env";
  * @returns
  */
 function MyApp({ Component, pageProps }: AppProps) {
+<<<<<<< HEAD
   const [env, setEnv] = useState<ResponseData>();
 
   const { chains, publicClient, webSocketPublicClient } = configureChains(
@@ -83,6 +116,31 @@ function MyApp({ Component, pageProps }: AppProps) {
         </WagmiConfig>
       )}
     </>
+=======
+
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        chains={chains}
+        coolMode={true}
+        locale='en'
+        showRecentTransactions={true}
+        theme={darkTheme({
+          accentColor: 'rgb(54 44 73 / var(--tw-bg-opacity)',
+          accentColorForeground: 'white',
+          borderRadius: 'medium',
+          fontStack: 'rounded',
+          overlayBlur: 'large',
+        })}
+        appInfo={{
+          appName: 'Monas',
+          learnMoreUrl: 'https://github.com/Monas-project/Filecoin-Data-Economy-Hackathon',
+        }}
+      >
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+>>>>>>> 30f3b97 (Add UI)
   );
 }
 
